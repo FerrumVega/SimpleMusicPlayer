885470afc4820e6675c2960f76ad5abee9fc2e44
FerrumVega <ferrumthevega@gmail.com>
2025-08-21 13:24:19 +0300

    большая обнова

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from yandex_music import Client
-from flask import Flask, send_file
-import io
+import flask
+import os
 import config
 
 client = Client(config.TOKEN).init()
-tracks = client.tracks([track_info.id for track_info in client.users_likes_tracks()])
-app = Flask(__name__)
+tracks = client.tracks(track_info.id for track_info in client.users_likes_tracks())
+sorted_tracks = sorted(tracks, key=lambda t: (t.title.lower(), t.artists_name()))
+sorted_tracks_ids = [track_info.id for track_info in sorted_tracks]
+app = flask.Flask(__name__)
 
 
-@app.route("/tracks/")
+@app.route("/")
 def index():
-    links = [
-        f'<a href="/track/{track.id}">{track.title} - {", ".join([artist.name for artist in track.artists])}</a>'
-        for track in tracks
-    ]
+    links = (
+        f'<a href="/track/{track_info.id}">{track_info.title} - {", ".join(track_info.artists_name())}</a>'
+        for track_info in sorted_tracks
+    )
     return "<br>".join(links)
 
 
 @app.route("/track/<track_id>/")
-def t(track_id):
-    track_bytes = client.tracks(track_id)[0].download_bytes()
-    return send_file(
-        io.BytesIO(track_bytes), mimetype="audio/mpeg", as_attachment=False
+def track_page(track_id):
+    track_info = client.tracks(track_id)[0]
+    song_full_name = f"{track_info.title} - {", ".join(track_info.artists_name())}"
+    song_path = f"static/{track_info.id}.mp3"
+    if not os.path.isfile(song_path):
+        track_info.download(song_path)
+    return flask.render_template(
+        "track_page.html",
+        song_full_name=song_full_name,
+        song_url=f"/{song_path}",
+        previous_track_url=f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) - 1]}",
+        next_track_url=f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) + 1]}",
     )
 
 
