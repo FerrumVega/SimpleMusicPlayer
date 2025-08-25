91cc240651aa6e0f4fe1fc6a788d7826442a3a94
FerrumVega <ferrumthevega@gmail.com>
2025-08-25 16:39:07 +0300

    внутренние изменения и повышение совместимости

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import yandex_music.exceptions
 import flask
 import os
 import config
-from PIL import Image
-import io
 
 
 def create_app():
@@ -51,18 +49,19 @@ def create_app():
             "main_page.html", tracks_names=return_string_of_track_links(sorted_tracks)
         )
 
-    @app.route("/track/<track_id>/like", methods=["POST"])
-    def like_track(track_id):
+    @app.route("/track/<track_id>/<type>", methods=["POST"])
+    def like_track(track_id, type):
         nonlocal liked_tracks_ids
         liked_tracks_ids = update_tracks_from_likes()[1]
-        client.tracks(track_id)[0].like()
-        return "", 204
-
-    @app.route("/track/<track_id>/dislike", methods=["POST"])
-    def dislike_track(track_id):
-        nonlocal liked_tracks_ids
+        if type == "like":
+            if track_id in liked_tracks_ids:
+                client.users_likes_tracks_remove(track_id)
+            else:
+                client.tracks(track_id)[0].like()
+        elif type == "dislike":
+            client.tracks(track_id)[0].dislike()
+            client.users_likes_tracks_remove(track_id)
         liked_tracks_ids = update_tracks_from_likes()[1]
-        client.tracks(track_id)[0].dislike()
         return "", 204
 
     @app.route("/feedback/<type>/", methods=["POST"])
@@ -130,19 +129,19 @@ def create_app():
                 previous_track_url = (
                     f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) - 1]}"
                 )
-            except ValueError:
+            except (ValueError, IndexError):
                 previous_track_url = ""
             try:
                 next_track_url = (
                     f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) + 1]}"
                 )
-            except ValueError:
+            except (ValueError, IndexError):
                 next_track_url = ""
         elif flask.request.args.get("from_query", "false") == "true":
             previous_track_url = ""
             try:
                 next_track_url = f"/track/{client.tracks_similar(track_id).similar_tracks[1].id}?from_query=true"
-            except IndexError:
+            except (ValueError, IndexError):
                 next_track_url = ""
         elif flask.request.args.get("from_wave", "false") == "true":
             client.rotor_station_feedback_track_started(
@@ -150,23 +149,23 @@ def create_app():
                 track_id=track_id,
                 batch_id=wave_station.batch_id,
             )
-            previous_track_url = ""
+            try:
+                previous_track_url = f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) - 1]}?from_wave=true"
+            except (ValueError, IndexError):
+                previous_track_url = ""
             try:
                 next_track_url = f"/track/{sorted_tracks_ids[sorted_tracks_ids.index(track_id) + 1]}?from_wave=true"
-            except IndexError:
+            except (ValueError, IndexError):
                 next_track_url = f"/my_wave?last_track_id={track_id}"
         if not os.path.isfile(song_path):
             track_info.download(song_path)
         if not os.path.isfile(cover_path):
-            cover = Image.open(io.BytesIO(track_info.download_cover_bytes("1080x1080")))
-            disk_mask = Image.open("static/disk_mask.png").convert("L")
-            disk_mask = disk_mask.resize(cover.size)
-            result = Image.new("RGBA", cover.size, (0, 0, 0, 0))
-            result.paste(cover, (0, 0), mask=disk_mask)
-            result.save(cover_path)
+            track_info.download_cover(cover_path, "1080x1080")
         return flask.render_template(
             "track_page.html",
             song_full_name=song_full_name,
+            song_title=track_info.title,
+            song_artists=", ".join(track_info.artists_name()),
             song_url=f"/{song_path}",
             previous_track_url=previous_track_url,
             next_track_url=next_track_url,
